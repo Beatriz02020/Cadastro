ed0a97ef89895aa71bd10189d8d1d2dc5acd1963
Beatriz02020 <biatriz9andrade@gmail.com>
2024-04-26 16:19:57 -0300

    pronto

---
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -0,0 +1,172 @@
+package com.example.cadastro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cadastro.ui.theme.CadastroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CadastroTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun App() {
+    Column(
+        Modifier
+            .background(Color.White)
+            .fillMaxWidth()
+            .padding(8.dp),
+        Arrangement.Center
+    ) {
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Text(text = "App Cadastro")
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            LabelAndPlaceHolder(
+                    Name = "Nome"
+            )
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            LabelAndPlaceHolder(
+                Name = "Endereço"
+            )
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            LabelAndPlaceHolder(
+                Name = "Bairro"
+            )
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            LabelAndPlaceHolder(
+                Name = "CEP"
+            )
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            LabelAndPlaceHolder(
+                Name = "Cidade"
+            )
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            LabelAndPlaceHolder(
+                Name = "Estado"
+            )
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Button(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Cadastrar")
+            }
+        }
+
+    }
+}
+
+@Composable
+fun LabelAndPlaceHolder(Name: String) {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = Name) },
+        placeholder = { Text(text = " ") },
+    )
+}
+@Composable
+@Preview
+fun AppPreview() {
+    CadastroTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            App()
+        }
+    }
+}
